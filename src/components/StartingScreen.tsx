25f376e23dc0841a13c64f241ec68626b131b1f3
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-09-17 18:17:14 -0500

    se agrega la imagen del logo

--- src/components/StartingScreen.tsx
+++ src/components/StartingScreen.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import LeanLogo from "/LeanTechLogo.png";
 
 export default function Example() {
   return (
@@ -20,16 +21,16 @@ export default function Example() {
         ))}
       </div>
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-        {/*<h1 className="text-6xl font-bold mb-4 text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500">
-          LEAN BUNNIES!!
-        </h1>
-*/}
-        <div className="relative w-64 h-64 mb-8">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full animate-pulse" />
+      <div className="relative z-10 flex flex-col items-center justify-start h-full text-white mt-8">
+        <div className="relative w-64 h-64 mb-8 ">
+          {/*<div className="absolute inset-0" />*/}
         </div>
-
-        <p className="text-xl mb-8 text-center max-w-md">
+        <img
+          src={LeanLogo}
+          alt="Cyborg Robot"
+          className="relative inset-0 w-64 h-64 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full animate-pulse"
+        />
+        <p className="text-xl mb-8 mt-8 text-center max-w-md">
           Put your skills to the test by answering questions about Lean Tech
           culture and much more!
         </p>
