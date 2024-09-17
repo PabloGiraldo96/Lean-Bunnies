ab151671dbc8efa5dbcadceb1f95d4026f114e4f
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-09-16 23:07:44 -0500

    se añade el react router para manejar salto entre componentes

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <div>
       <Example />
       <LoginScreenComponent />
-      <QuestionsComponent />
-      <ScoreboardComponent />
+      {/*<QuestionsComponent />*/}
+      {/*<ScoreboardComponent />*/}
     </div>
   );
 }
